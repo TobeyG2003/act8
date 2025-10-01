09c71322b7d93d6c886ef6fe1952472b7e9cc230
Tobey <TobeyG2003@gmail.com>
2025-10-01 18:41:38 -0400

    allows theme changing

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void changeTheme(ThemeMode mode) {
+    setState(() {
+      _themeMode = mode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: FadingTextAnimation(
+        changeTheme: changeTheme,
+        themeMode: _themeMode,
+      ),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
+  final void Function(ThemeMode) changeTheme;
+  final ThemeMode themeMode;
+
+  const FadingTextAnimation({Key? key, required this.changeTheme, required this.themeMode}) : super(key: key);
+
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
-
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
 
@@ -29,46 +52,59 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2,
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text('Fading Text Animation'),
-        bottom: TabBar(
-        tabs: [
-          Tab(text: 'Hello, Flutter!'),
-          Tab(text: 'Sample'),
-        ],
-      ),
-      ),
-      body: TabBarView(
-        children: [
-       Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+    final isDark = widget.themeMode == ThemeMode.dark;
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: ElevatedButton(
+            onPressed: () {
+              if (isDark) {
+                widget.changeTheme(ThemeMode.light);
+              } else {
+                widget.changeTheme(ThemeMode.dark);
+              }
+            },
+            child: Icon(widget.themeMode == ThemeMode.dark ? Icons.nightlight_round : Icons.wb_sunny)
+          ),
+          title: const Text('Fading Text Animation'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Hello, Flutter!'),
+              Tab(text: 'Sample'),
+            ],
           ),
         ),
-      ),
-      Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 5),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 1),
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+            Center(
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 5),
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: toggleVisibility,
+          child: const Icon(Icons.play_arrow),
         ),
       ),
-        ]
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
-      ),
-    )
     );
   }
 }

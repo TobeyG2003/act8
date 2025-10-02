5d826cdbb0efa0494d092b79dd83f8b0449d7b7a
Tobey <TobeyG2003@gmail.com>
2025-10-01 22:03:46 -0400

    adds color picker and new tab with image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -43,6 +44,7 @@ class FadingTextAnimation extends StatefulWidget {
 }
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color selectedTextColor = Colors.blue;
 
   void toggleVisibility() {
     setState(() {
@@ -54,11 +56,13 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   Widget build(BuildContext context) {
     final isDark = widget.themeMode == ThemeMode.dark;
 
+    bool isSwitched = false;
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
-          leading: ElevatedButton(
+          actions: <Widget> [
+            ElevatedButton(
             onPressed: () {
               if (isDark) {
                 widget.changeTheme(ThemeMode.light);
@@ -68,11 +72,34 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
             },
             child: Icon(widget.themeMode == ThemeMode.dark ? Icons.nightlight_round : Icons.wb_sunny)
           ),
+          ElevatedButton(
+                  onPressed: () async {
+                    final newColor = await showColorPickerDialog(
+                      context,
+                      selectedTextColor,
+                      title: Text('Pick a Color', style: Theme.of(context).textTheme.headlineSmall),
+                      pickersEnabled: const <ColorPickerType, bool>{
+                        ColorPickerType.both: false,
+                        ColorPickerType.primary: true,
+                        ColorPickerType.accent: true,
+                        ColorPickerType.bw: false,
+                        ColorPickerType.custom: true,
+                        ColorPickerType.wheel: true,
+                      },
+                    );
+                    setState(() {
+                      selectedTextColor = newColor;
+                    });
+                  },
+                  child: Icon(Icons.color_lens, color: selectedTextColor),
+                ),
+          ],
           title: const Text('Fading Text Animation'),
           bottom: const TabBar(
             tabs: [
               Tab(text: 'Hello, Flutter!'),
-              Tab(text: 'Sample'),
+              Tab(text: 'Bounce In'),
+              Tab(text: 'Image'),
             ],
           ),
         ),
@@ -82,9 +109,10 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
               child: AnimatedOpacity(
                 opacity: _isVisible ? 1.0 : 0.0,
                 duration: const Duration(seconds: 1),
-                child: const Text(
+                curve: Curves.easeInOut,
+                child: Text(
                   'Hello, Flutter!',
-                  style: TextStyle(fontSize: 24),
+                  style: TextStyle(fontSize: 24, color: selectedTextColor),
                 ),
               ),
             ),
@@ -92,11 +120,31 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
               child: AnimatedOpacity(
                 opacity: _isVisible ? 1.0 : 0.0,
                 duration: const Duration(seconds: 5),
-                child: const Text(
-                  'Hello, Flutter!',
-                  style: TextStyle(fontSize: 24),
+                curve: Curves.bounceIn,
+                child: Text(
+                  'This Text bounces in',
+                  style: TextStyle(fontSize: 24, color: selectedTextColor),
                 ),
               ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://images.steamusercontent.com/ugc/2056503000594664135/4A6CC88DE96CE4779E62810DCE348F5241999F59/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                  width: 300,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+                Switch(
+                  value: isSwitched,
+                  onChanged: (newValue) {
+                  setState(() {
+                  isSwitched = newValue; // Update the state when the switch is toggled
+                });
+              },
+            ),
+              ],
             ),
           ],
         ),
